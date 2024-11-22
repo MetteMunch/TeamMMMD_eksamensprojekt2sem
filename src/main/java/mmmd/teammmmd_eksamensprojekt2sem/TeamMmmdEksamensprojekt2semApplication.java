b756d9259adfd1c4fe223a9f058ce617af8059bb
MetteMunch <meha0005@stud.kea.dk>
2024-11-22 12:26:20 +0100

    #46 Projekt oprettet med package

---
+++ src/main/java/mmmd/teammmmd_eksamensprojekt2sem/TeamMmmdEksamensprojekt2semApplication.java
@@ -0,0 +1,13 @@
+package mmmd.teammmmd_eksamensprojekt2sem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamMmmdEksamensprojekt2semApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamMmmdEksamensprojekt2semApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mmmd/teammmmd_eksamensprojekt2sem/TeamMmmdEksamensprojekt2semApplicationTests.java
@@ -0,0 +1,13 @@
+package mmmd.teammmmd_eksamensprojekt2sem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamMmmdEksamensprojekt2semApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
