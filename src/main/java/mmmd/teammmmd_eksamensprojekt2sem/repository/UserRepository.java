01b0517edadb7414491366fef40ff668b208c5f2
DanielJensenKEA <daje0003@stud.kea.dk>
2024-11-23 11:26:37 +0100

    Tilføjet databaseforbindelse. Har slettet testmetode, men det virker.

---
+++ src/main/java/mmmd/teammmmd_eksamensprojekt2sem/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package mmmd.teammmmd_eksamensprojekt2sem.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+    public UserRepository() {
+
+    }
+}
