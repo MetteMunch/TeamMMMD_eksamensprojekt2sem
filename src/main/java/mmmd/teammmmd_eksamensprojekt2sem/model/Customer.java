170c69df27cfd53f9c1acdd60b8d1e19cef982d5
DanielJensenKEA <daje0003@stud.kea.dk>
2024-11-25 14:52:03 +0100

    Customer model med metoder til at create og set id

---
+++ src/main/java/mmmd/teammmmd_eksamensprojekt2sem/model/Customer.java
@@ -0,0 +1,29 @@
+package mmmd.teammmmd_eksamensprojekt2sem.model;
+
+public class Customer {
+    private int customerID;
+    private String companyName;
+    private String repName;
+
+    public Customer(String companyName, String repName) {
+        customerID = -1;
+        this.companyName = companyName;
+        this.repName = repName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getRepName() {
+        return repName;
+    }
+
+    public void setRepName(String repName) {
+        this.repName = repName;
+    }
+}
--- src/main/java/mmmd/teammmmd_eksamensprojekt2sem/repository/ProjectRepository.java
+++ src/main/java/mmmd/teammmmd_eksamensprojekt2sem/repository/ProjectRepository.java
@@ -1,8 +1,10 @@
 package mmmd.teammmmd_eksamensprojekt2sem.repository;
+import mmmd.teammmmd_eksamensprojekt2sem.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 
@@ -15,6 +17,29 @@ public class ProjectRepository {
     public ProjectRepository(ConnectionManager connectionManager) throws SQLException {
         this.connectionManager = connectionManager;
         connection = connectionManager.getConnection();
+    }
+
+    public void createCustomer(Customer customer) {
+        String sql="INSERT INTO customer(companyName, repName) VALUES(?,?)";
+        try(PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, customer.getCompanyName());
+            ps.setString(2, customer.getRepName());
+            ps.executeUpdate();
+
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public int lookUpCustomerIDFromDB(Customer customer) {
+        String sql="SELECT customerID FROM customer WHERE companyName=? AND repName=?";
+        try(PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, customer.getCompanyName());
+            ps.setString(2, customer.getRepName());
+
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
