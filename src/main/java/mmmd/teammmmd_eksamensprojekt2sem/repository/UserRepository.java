64638ce2c8ea43c920faad60611ea63ec81b56d2
MetteMunch <meha0005@stud.kea.dk>
2024-11-26 15:04:14 +0100

    Færdiggjort login med validering af bruger. Der er lavet test af tilknyttede metoder i repository (ikke Controller endnu). Endpoint kan redirecte til korrekt view alt efter om employee er manager eller ej, men mangler de korrekte sti'er til dette. #28 #27

--- src/main/java/mmmd/teammmmd_eksamensprojekt2sem/repository/UserRepository.java
+++ src/main/java/mmmd/teammmmd_eksamensprojekt2sem/repository/UserRepository.java
@@ -3,6 +3,7 @@ package mmmd.teammmmd_eksamensprojekt2sem.repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.xml.transform.Result;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -18,18 +19,49 @@ public class UserRepository {
         this.dbConnection = connectionManager.getConnection();
     }
 
-    //LOGIN METHODS - USER STORY ISSUE 22 - CREATED BY: Mette,
+    //LOGIN METHODS - CREATED BY: Mette,
 
     public boolean validateLogin(String username, String password) throws SQLException {
         boolean result = false;
         String SQL = "SELECT password FROM employee WHERE username =?";
 
-        try(PreparedStatement ps = dbConnection.prepareStatement(SQL)) {
-            ps.setString(1,username);
-            try(ResultSet rs = ps.executeQuery()) {
-                if(rs.next()) {
+        try (PreparedStatement ps = dbConnection.prepareStatement(SQL)) {
+            ps.setString(1, username);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
                     String dbPassword = rs.getString("password");
                     result = dbPassword.equals(password);
+                }
+            }
+        }
+        return result;
+    }
+
+    public int getEmployeeIdFromDB(String username) throws SQLException {
+        int result = 0;
+        String SQL = "SELECT employeeID FROM employee WHERE username =?";
+
+        try (PreparedStatement ps = dbConnection.prepareStatement(SQL)) {
+            ps.setString(1, username);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    result = rs.getInt("employeeID");
+                }
+            }
+        }
+        return result;
+    }
+
+    //Muligvis kan nedenstående metode erstattes af alm getter metode fra model senere?
+    public boolean getIsEmployeeManagerInfoFromDB(int employeeID) throws SQLException {
+        boolean result = false;
+        String SQL = "SELECT isManager FROM employeerole INNER JOIN employee ON employee.role = employeerole.roleid WHERE employeeID =?";
+
+        try (PreparedStatement ps = dbConnection.prepareStatement(SQL)) {
+            ps.setInt(1,employeeID);
+            try (ResultSet rs = ps.executeQuery()) {
+                if(rs.next()) {
+                    result = rs.getBoolean("isManager");
                 }
             }
         }
--- src/main/java/mmmd/teammmmd_eksamensprojekt2sem/service/UserService.java
+++ src/main/java/mmmd/teammmmd_eksamensprojekt2sem/service/UserService.java
@@ -4,6 +4,8 @@ import mmmd.teammmmd_eksamensprojekt2sem.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
+
 @Service
 public class UserService {
 
@@ -13,5 +15,17 @@ public class UserService {
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
 
+    }
+
+    public boolean validateLogin(String username, String password) throws SQLException {
+        return userRepository.validateLogin(username, password);
+    }
+
+    public int getEmployeeIDFromDB(String username) throws SQLException {
+        return userRepository.getEmployeeIdFromDB(username);
+    }
+
+    public boolean getIsEmployeeManagerInfoFromDB(int employeeID) throws SQLException {
+        return userRepository.getIsEmployeeManagerInfoFromDB(employeeID);
     }
 }
