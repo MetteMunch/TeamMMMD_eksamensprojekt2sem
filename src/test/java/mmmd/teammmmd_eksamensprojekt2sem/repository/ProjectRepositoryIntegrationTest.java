b3593cc463d23a22d97c59f69e6e26306e4cc262
DanielJensenKEA <daje0003@stud.kea.dk>
2024-11-28 16:51:01 +0100

    Integrationstest. 10.

--- src/test/java/mmmd/teammmmd_eksamensprojekt2sem/repository/ProjectRepositoryIntegrationTest.java
+++ src/test/java/mmmd/teammmmd_eksamensprojekt2sem/repository/ProjectRepositoryIntegrationTest.java
@@ -1,6 +1,7 @@
 package mmmd.teammmmd_eksamensprojekt2sem.repository;
 
 import mmmd.teammmmd_eksamensprojekt2sem.model.Customer;
+import mmmd.teammmmd_eksamensprojekt2sem.model.Project;
 import org.h2.tools.RunScript;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -39,6 +41,11 @@ public class ProjectRepositoryIntegrationTest {
         Connection connection = connectionManager.getConnection(); //Her tester vi om der er forbindelse til H2 databasen
         assertNotNull(connection, "Forbindelse til H2-testdatabasen burde ikke være null");
     }
+    /*
+    #####################################
+    #           CRUD Customer           #
+    #####################################
+     */
     @Test
     void getListOfCurrentCustomers() throws Exception {
         List<Customer> customersToReturn = projectRepository.getListOfCurrentCustomers();
@@ -69,4 +76,104 @@ public class ProjectRepositoryIntegrationTest {
         //Assert
         assertEquals(expectedSize, actualSize);
     }
+    @Test
+    void lookUpCustomerIDFromDB() {
+        //Arrange
+        Customer customer = new Customer("TechCorp", "Emily White"); //Eksisterer i forvejen i H2.
+
+        //Act
+        int expectedCustomerID = 1; //Customer er den første, der bliver indsat i h2, må derfor være nr. 1.
+        int actualcustomerId = projectRepository.lookUpCustomerIDFromDB(customer);
+
+        //Assert
+        assertEquals(expectedCustomerID, actualcustomerId);
+    }
+    @Test
+    void lookUpCustomerIDFromDBFail() {
+        //Arrange
+        Customer customer = new Customer("TechCorp", "Emily White"); //Eksisterer i forvejen i H2.
+
+        //Act
+        int expectedCustomerID = 2; //Customer er den første, der bliver indsat i h2, må derfor være nr. 1. Sætter 2 for at faile testen.
+        int actualcustomerId = projectRepository.lookUpCustomerIDFromDB(customer);
+
+        //Assert
+        assertNotEquals(expectedCustomerID, actualcustomerId);
+    }
+
+    /*
+    #####################################
+    #           CRUD Project            #
+    #####################################
+     */
+    @Test
+    void createProject(){
+        //Arrange
+        Project newProject = new Project("Project Title Test", "Project Description", 1, Date.valueOf("2024-12-12"), Date.valueOf("2025-01-01"), "Link", 2, 1 );
+        int expectedNumOfProjects = 3; //2 eksisterende projekter. +1 for newProject.
+
+        //Act
+        projectRepository.createProject(newProject); //Liste skulle gerne vokse med +1 efter createProject(), da projektet tilføjes til databasen.
+        List<Project> list = projectRepository.showAllProjects();
+        int actualNumOfProjects = list.size();
+
+        //Assert
+        assertEquals(expectedNumOfProjects, actualNumOfProjects);
+    }
+    @Test
+    void showAllProjects() {
+        //Arrange
+        List<Project> list = projectRepository.showAllProjects();
+        int expectedNumOfProjects = 2; //Eksisterer 2 projekter i h2 sql insert fil.
+
+        //Act
+        int actualNumOfProjects = list.size();
+
+        //Assert
+        assertEquals(expectedNumOfProjects, actualNumOfProjects);
+    }
+    @Test
+    void updateProject() {
+        //Arrange
+        Project newProject = new Project("Project Title Test", "Project Description",
+                1, Date.valueOf("2024-12-12"), Date.valueOf("2025-01-01"), "Link",
+                2, 1 ); //Vi laver et helt nyt projekt for at bruge vores createMetode.
+        //Act
+        projectRepository.createProject(newProject); //Tilføjer det til databasen.
+        projectRepository.setProjectID(newProject); //Bemærk, ikke en almindelige setter metode. Laver et lookup i DB for at finde autogenererede ID.
+
+        Project updateProject = projectRepository.fetchSpecificProject("Project Title Test"); //Vi henter vores projekt i DB efter det er oprettet.
+        updateProject.setProjectDescription("Updated Description"); //Vi opdaterer beskrivelsen.
+        projectRepository.updateProject(updateProject); //Vores update tager et Project objekt, så når vi har opdateret de ønskede attributter sender vi det nye og opdaterede objekt til DB.
+
+        Project newProjectUpdated = projectRepository.fetchSpecificProject("Project Title Test"); //Vi finder det opdaterede projekt frem ved at søge på dets titel.
+
+        //Assert
+        assertEquals(newProject.getID(), updateProject.getID()); //Tjek af samme ID.
+        assertNotEquals(newProject.getProjectDescription(), updateProject.getProjectDescription());
+        assertEquals("Updated Description", newProjectUpdated.getProjectDescription()); //Vi tjekker om opdatering er gået igennem.
+    }
+    @Test
+    void deleteProject() throws Exception {
+        //Arrange
+        Project newProject = new Project("Project Title Test", "Project Description",
+                1, Date.valueOf("2024-12-12"), Date.valueOf("2025-01-01"), "Link",
+                2, 1 );
+
+        //Act
+        projectRepository.createProject(newProject);
+        projectRepository.setProjectID(newProject);
+        List<Project> list = projectRepository.showAllProjects();
+        int actualSizeAfterInsertion = list.size();
+        int expectedSizeAfterInsertion = 2+1; //2 projekter eksisterer allerede i h2. +1 efter insertion.
+            assertEquals(expectedSizeAfterInsertion, actualSizeAfterInsertion);
+        projectRepository.deleteProject(newProject);
+        List<Project> listDeletion = projectRepository.showAllProjects();
+        int actualSizeAfterDeletion = listDeletion.size();
+        int expectedSizeAfterDeletion = 3-1; //3 projekter før deletion. -1 efter deletion.
+
+        //Assert
+        assertEquals(actualSizeAfterDeletion, expectedSizeAfterDeletion);
+    }
+
 }
